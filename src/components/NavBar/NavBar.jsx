449b9fbe34acba3b61375102e6730c29355afdad
BlasCasale <117787465+BlasCasale@users.noreply.github.com>
2023-10-23 16:29:19 -0300

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = ({ style, changeDarkMode }) => {
 
                 <a href="https://github.com/BlasCasale" target='_blank'><img src="/gh.webp" alt="GitHub" /></a>
 
-                <a href="https://www.linkedin.com/in/blas-casale-987716259/" target='_blank'><img src="/li.webp" alt="LinkedIn" /></a>
+                <a href="https://www.linkedin.com/in/blas-casale/" target='_blank'><img src="/li.webp" alt="LinkedIn" /></a>
 
                 <a href="https://wa.me/5492214097316?text=Hola%20Blas,%20revise%20tu%20portfolio%20y%20estoy%20interesado%20en%20contactarte%20de%20parte%20de%20..." target='_blank'><img src="/wsp.webp" alt="WhatsApp" /></a>
 
