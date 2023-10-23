c7dbad93e94ca00a8af6e31c0daecfeadf66da5a
BlasCasale <117787465+BlasCasale@users.noreply.github.com>
2023-10-23 16:38:01 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = ({ style }) => {
                 
                 <i className={`bi bi-geo-alt ${style}Info`}> Argentina, Buenos Aires, La Plata.</i>
 
-                <i className={`bi bi-file-earmark-arrow-down ${style}Info`}><a href="https://drive.google.com/file/d/1bsSBZCupCGyXXtWbtzAYC-zQH4qdLY-I/view?usp=share_link" target='_blank'> CV</a></i>
+                <i className={`bi bi-file-earmark-arrow-down ${style}Info`}><a href="https://drive.google.com/file/d/1rOjoShh_CxqjoQPn20n6Bv19nkbYandc/view?usp=share_link" target='_blank'> CV</a></i>
 
             </footer>
         </div>
